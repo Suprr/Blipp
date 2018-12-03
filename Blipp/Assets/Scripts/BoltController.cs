ec1df7abaf3b2135bbb21062e6c76d05fa177651
StevenZabolotny <stevezabolotny@gmail.com>
2018-12-03 04:49:38 -0500

    Added Bolt model and made bolts spawn, fall and collide with cubes

---
+++ Blipp/Assets/Scripts/BoltController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoltController : MonoBehaviour {
+
+    public float fallSpeed;
+
+    private Color color;
+
+	// Use this for initialization
+	void Start () {
+        Color[] colors = new Color[4];
+        colors[0] = new Color(1.0f, 0.0f, 0.0f, 1.0f); // Red
+        colors[1] = new Color(0.0f, 1.0f, 0.0f, 1.0f); // Green
+        colors[2] = new Color(0.0f, 0.0f, 1.0f, 1.0f); // Blue
+        colors[3] = new Color(1.0f, 1.0f, 0.0f, 1.0f); // Yellow
+
+        MeshRenderer renderer = this.gameObject.GetComponent<MeshRenderer>();
+        renderer.materials[0].color = new Color(1.0f, 1.0f, 0.0f, 1.0f);
+        int r = Random.Range(0, 4);
+        renderer.materials[0].color = colors[r]; // Choose a random color and change the color material to it
+        color = colors[r];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = transform.position - Time.deltaTime * fallSpeed * new Vector3(0, 1, 0); // Make the bolt fall at the fall speed
+	}
+
+    void OnTriggerStay(Collider collision)
+    {
+        GameObject cube = collision.gameObject;
+        MeshRenderer renderer = cube.GetComponent<MeshRenderer>();
+        if (renderer.materials.Length > 1 && renderer.materials[1].color != color) // Get the color of the cube the bolt has collided with
+        {
+            Destroy(this.gameObject); // If the colors are different, destroy the bolt
+        }
+    }
+}
---
+++ Blipp/Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject bolt;
+    public int nBolts;
+    public int timeBetweenBolts;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0;i < nBolts;i++)
+        {
+            Invoke("SpawnBolt", timeBetweenBolts * i); // Call SpawnBolt nBolts times, with a delay of timeBetweenBolts between each call
+        }
+	}
+
+    // Spawn a new Bolt
+    void SpawnBolt()
+    {
+        GameObject newBolt = Instantiate(bolt); // Copy the bolt
+        newBolt.transform.SetParent(bolt.transform.parent);
+        int r1 = Random.Range(-1, 2); 
+        int r2 = Random.Range(-1, 2);
+        newBolt.transform.position = new Vector3(newBolt.transform.position.x + r1 * 2, newBolt.transform.position.y, newBolt.transform.position.z + r2 * 2); // Randomize the position
+        // Color is randomized in BoltController Start()
+        newBolt.SetActive(true); // Set the bolt to active
+    }
+}
