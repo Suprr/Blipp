d9f70a54c0eb6e39f2223d384d1118a2ad775885
StevenZabolotny <stevezabolotny@gmail.com>
2018-12-11 00:00:09 -0500

    Added score, combo, and lives text display, bolts now despawn after successful pass

--- Blipp/Assets/Scripts/BoltController.cs
+++ Blipp/Assets/Scripts/BoltController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class BoltController : MonoBehaviour {
 
+    public float spawnHeight; // Spawning height of bolt
     public float fallSpeed; // Falling speed of bolt
     public GameObject[] circleSprites; // Array of Circle Sprite Objects to copy
     public Color[] colors; // Array of Colors
+    public GameController gamecontroller;
 
     private int rand; // Random number chosen for colors
     private Color color; // Random color chosen for bolt
@@ -21,6 +23,7 @@ public class BoltController : MonoBehaviour {
         color = colors[r];
 
         rand = r;
+        transform.position = new Vector3(transform.position.x, spawnHeight, transform.position.z);
     }
 
     // Create a Circle Sprite given the X and Y
@@ -44,7 +47,13 @@ public class BoltController : MonoBehaviour {
         transform.position = transform.position - Time.deltaTime * fallSpeed * new Vector3(0, 1, 0); // Make the bolt fall at the fall speed
         if (transform.position.y >= 3) // If the bolt has not reached the cube, make the circle sprite grow in size based on how far the bolt has fallen
         {
-            rectTransform.sizeDelta = new Vector2(40 - 4 * (transform.position.y - 3), 40 - 4 * (transform.position.y - 3));
+            rectTransform.sizeDelta = new Vector2(40 - (40 / spawnHeight) * (transform.position.y - 3), 40 - (40 / spawnHeight) * (transform.position.y - 3));
+        }
+        if (transform.position.y <= -4)
+        {
+            gamecontroller.AddScore(); // Make the player gain score
+            Destroy(circleSprite);
+            Destroy(this.gameObject); // The bolt has made it through the cube so destroy it
         }
     }
 
@@ -54,6 +63,7 @@ public class BoltController : MonoBehaviour {
         MeshRenderer renderer = cube.GetComponent<MeshRenderer>();
         if (renderer.materials.Length > 1 && renderer.materials[1].color != color) // Get the color of the cube the bolt has collided with
         {
+            gamecontroller.LoseLife(); // Make the player lose a life
             Destroy(circleSprite);
             Destroy(this.gameObject); // If the colors are different, destroy the bolt
         }
--- Blipp/Assets/Scripts/GameController.cs
+++ Blipp/Assets/Scripts/GameController.cs
@@ -1,15 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
-    public GameObject bolt;
-    public int nBolts;
-    public int timeBetweenBolts;
+    public GameObject bolt; // Original bolt game object
+    public int nBolts; // Number of bolts
+    public int timeBetweenBolts; // Time between bolts
+    public Text scoreText; // Score Text
+    public Text livesText; // Lives Text
+    public Text comboText; // Combo Text
+    public int scoreValue; // Value that each bolt gives, scorewise
+
+    private int score; // Current score
+    private int combo; // Current combo
+    private int lives; // Current lives
 
 	// Use this for initialization
 	void Start () {
+        score = 0;
+        combo = 1;
+        lives = 3;
 		for (int i = 0;i < nBolts;i++)
         {
             Invoke("SpawnBolt", timeBetweenBolts * i); // Call SpawnBolt nBolts times, with a delay of timeBetweenBolts between each call
@@ -29,5 +41,30 @@ public class GameController : MonoBehaviour {
         newBolt.transform.position = new Vector3(newBolt.transform.position.x + r1 * 2, newBolt.transform.position.y, newBolt.transform.position.z + r2 * 2); // Randomize the position
         // Color is randomized in BoltController Start()
         newBolt.SetActive(true); // Set the bolt to active
+    }
+
+    // Add to score after successful bolt
+    public void AddScore()
+    {
+        score += scoreValue * combo; // Multiply score value by combo
+        combo++; // Increment combo
+        scoreText.text = "Score: " + score.ToString("000000"); // Update Score and Combo Text
+        comboText.text = "Combo: " + combo.ToString() + "x";
+    }
+
+    // Lose life due to missed bolt
+    public void LoseLife()
+    {
+        lives--; // Decrement lives
+        combo = 1; // Reset combo
+        livesText.text = "Lives: " + lives; // Update lives and combo text
+        comboText.text = "Combo: " + combo.ToString() + "x";
+        if (lives == 2) // Change color of lives based on number of lives remaining
+        {
+            livesText.color = new Color(1.0f, 1.0f, 0.0f, 1.0f);
+        } else if (lives == 1)
+        {
+            livesText.color = new Color(1.0f, 0.0f, 0.0f, 1.0f);
+        }
     }
 }
