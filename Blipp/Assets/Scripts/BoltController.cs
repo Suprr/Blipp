3342096fbe0333685aa6500441c7533b2c57decf
StevenZabolotny <stevezabolotny@gmail.com>
2018-12-10 23:15:26 -0500

    Added UI top view element to track where the bolts are falling from a top-down view

--- Blipp/Assets/Scripts/BoltController.cs
+++ Blipp/Assets/Scripts/BoltController.cs
@@ -4,29 +4,49 @@ using UnityEngine;
 
 public class BoltController : MonoBehaviour {
 
-    public float fallSpeed;
+    public float fallSpeed; // Falling speed of bolt
+    public GameObject[] circleSprites; // Array of Circle Sprite Objects to copy
+    public Color[] colors; // Array of Colors
 
-    private Color color;
-
-	// Use this for initialization
-	void Start () {
-        Color[] colors = new Color[4];
-        colors[0] = new Color(1.0f, 0.0f, 0.0f, 1.0f); // Red
-        colors[1] = new Color(0.0f, 1.0f, 0.0f, 1.0f); // Green
-        colors[2] = new Color(0.0f, 0.0f, 1.0f, 1.0f); // Blue
-        colors[3] = new Color(1.0f, 1.0f, 0.0f, 1.0f); // Yellow
-
+    private int rand; // Random number chosen for colors
+    private Color color; // Random color chosen for bolt
+    private GameObject circleSprite; // Random circle sprite chosen for bolt
+    private RectTransform rectTransform; // Rect Transform of the Circle Sprite
+	
+    public void SetColor(int r)
+    {
         MeshRenderer renderer = this.gameObject.GetComponent<MeshRenderer>();
         renderer.materials[0].color = new Color(1.0f, 1.0f, 0.0f, 1.0f);
-        int r = Random.Range(0, 4);
         renderer.materials[0].color = colors[r]; // Choose a random color and change the color material to it
         color = colors[r];
+
+        rand = r;
     }
-	
+
+    // Create a Circle Sprite given the X and Y
+    public void CreateCircleSprite(int x, int y)
+    {
+        GameObject newCircleSprite = Instantiate(circleSprites[rand]); // Copy the circle sprite
+        newCircleSprite.transform.SetParent(circleSprites[rand].transform.parent);
+
+        int newX = 86 + 49 * x; // Calculate the position on the UI relative to X and Y
+        int newY = -1 * (86 - 49 * y);
+        rectTransform = newCircleSprite.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = new Vector3(newX, newY, 0); // Change the position and initialize size
+        rectTransform.sizeDelta = new Vector2(12, 12);
+
+        newCircleSprite.SetActive(true); // Set Active
+        circleSprite = newCircleSprite;
+    }
+
 	// Update is called once per frame
 	void Update () {
         transform.position = transform.position - Time.deltaTime * fallSpeed * new Vector3(0, 1, 0); // Make the bolt fall at the fall speed
-	}
+        if (transform.position.y >= 3) // If the bolt has not reached the cube, make the circle sprite grow in size based on how far the bolt has fallen
+        {
+            rectTransform.sizeDelta = new Vector2(40 - 4 * (transform.position.y - 3), 40 - 4 * (transform.position.y - 3));
+        }
+    }
 
     void OnTriggerStay(Collider collision)
     {
@@ -34,6 +54,7 @@ public class BoltController : MonoBehaviour {
         MeshRenderer renderer = cube.GetComponent<MeshRenderer>();
         if (renderer.materials.Length > 1 && renderer.materials[1].color != color) // Get the color of the cube the bolt has collided with
         {
+            Destroy(circleSprite);
             Destroy(this.gameObject); // If the colors are different, destroy the bolt
         }
     }
--- Blipp/Assets/Scripts/GameController.cs
+++ Blipp/Assets/Scripts/GameController.cs
@@ -23,6 +23,9 @@ public class GameController : MonoBehaviour {
         newBolt.transform.SetParent(bolt.transform.parent);
         int r1 = Random.Range(-1, 2); 
         int r2 = Random.Range(-1, 2);
+        int r = Random.Range(0, 4); // Choose a new random color
+        newBolt.GetComponent<BoltController>().SetColor(r); // Pass the randomly chosen values to the bolt
+        newBolt.GetComponent<BoltController>().CreateCircleSprite(r1, r2);
         newBolt.transform.position = new Vector3(newBolt.transform.position.x + r1 * 2, newBolt.transform.position.y, newBolt.transform.position.z + r2 * 2); // Randomize the position
         // Color is randomized in BoltController Start()
         newBolt.SetActive(true); // Set the bolt to active
