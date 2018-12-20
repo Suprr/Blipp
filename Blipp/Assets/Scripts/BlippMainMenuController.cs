14e7cad43f45d821f87966b9c397736ee2de67f3
Suprr <michaelrizzo1996@gmail.com>
2018-12-20 13:38:24 -0500

    added some final touches w.r.t. sound and various visual aspects

--- Blipp/Assets/Scripts/BlippMainMenuController.cs
+++ Blipp/Assets/Scripts/BlippMainMenuController.cs
@@ -8,12 +8,28 @@ public class BlippMainMenuController : MonoBehaviour {
 
     public Button arcade;
     public Button levels;
+    public AudioSource audiosource;
+    public AudioClip blipp;
 
     // Use this for initialization
     void Start() {
         arcade.onClick.AddListener(Arcade); // Add listener to button
         levels.onClick.AddListener(Levels); // Add listener to button
+        StartCoroutine(PlayAudio (3));
     }
+ 
+    IEnumerator PlayAudio(int times)
+    {
+        for(int i=0; i<times; i++)
+        {
+            audiosource.PlayOneShot(blipp, 0.5F);
+            yield return new WaitForSeconds(blipp.length-0.095f);
+        }
+    }
+    
+    void Update(){
+    }
+
 
     void Arcade()
     {
--- Blipp/Assets/Scripts/LevelMenuController.cs
+++ Blipp/Assets/Scripts/LevelMenuController.cs
@@ -9,14 +9,28 @@ public class LevelMenuController : MonoBehaviour {
     public Button levelOne;
     public Button levelTwo;
     public Button back;
+    public AudioSource audiosource;
+    public AudioClip menu;
+    public AudioClip ret;
+
 
 	// Use this for initialization
 	void Start () {
         levelOne.onClick.AddListener(LevelOne); // Add listener to button
         levelTwo.onClick.AddListener(LevelTwo); // Add listener to button
         back.onClick.AddListener(Back);
+        StartCoroutine(Menu(1));
     }
 	
+    IEnumerator Menu(int times)
+    {
+        for(int i=0; i<times; i++)
+        {
+            audiosource.PlayOneShot(menu, 0.5F);
+            yield return new WaitForSeconds(menu.length);
+        }
+    }
+
     void LevelOne()
     {
         LevelData.LevelOne();
@@ -30,6 +44,16 @@ public class LevelMenuController : MonoBehaviour {
     }
 
     void Back() {
+        StartCoroutine(Ret(1));
+    }
+    
+    IEnumerator Ret(int times)
+    {
+        for(int i=0; i<times; i++)
+        {
+            audiosource.PlayOneShot(ret, 0.5F);
+            yield return new WaitForSeconds(ret.length*1.5f);
+        }
         SceneManager.LoadScene("MainMenu"); // Go back to Main Menu
     }
 }
